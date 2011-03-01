8a17da1ab2de616b130655e6ad33f131e7a36bfa
Joe Sak <joe@joesak.com>
2011-02-28 21:34:47 -0600

    Contact

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110301031316) do
+ActiveRecord::Schema.define(:version => 20110301033433) do
 
   create_table "images", :force => true do |t|
     t.string   "image_mime_type"
@@ -22,6 +22,26 @@ ActiveRecord::Schema.define(:version => 20110301031316) do
     t.datetime "updated_at"
     t.string   "image_uid"
     t.string   "image_ext"
+  end
+
+  create_table "inquiries", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.text     "message"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "spam",       :default => false
+  end
+
+  add_index "inquiries", ["id"], :name => "index_inquiries_on_id"
+
+  create_table "inquiry_settings", :force => true do |t|
+    t.string   "name"
+    t.text     "value"
+    t.boolean  "destroyable"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "ladies", :force => true do |t|
