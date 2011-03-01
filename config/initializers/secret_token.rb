77f59699dcbe1458d09f12d1a6a4342bed65b4fe
Joe Sak <joe@joesak.com>
2011-02-28 20:33:24 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Niceracks::Application.config.secret_token = 'f7f716cac41adcb3aa358ba53a8bc5e5fee7ab060f4cbbd0e445ab6c81f6e034e2db39b159b65cbf5f3f1bc17a2346156284d4f5d4b52556cc8030a2c14c88a2'
