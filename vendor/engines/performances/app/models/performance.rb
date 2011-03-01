09e3e7b2cfeebd0852fadfa2bad7a1cfbfa4b497
Joe Sak <joe@joesak.com>
2011-02-28 21:11:21 -0600

    Performances sort and appear by relevant date, some formatting of feeds

--- vendor/engines/performances/app/models/performance.rb
+++ vendor/engines/performances/app/models/performance.rb
@@ -1,10 +1,16 @@
 class Performance < ActiveRecord::Base
+  
+  default_scope where(['ends_at < ?', Time.now]).order('starts_at ASC')
 
   acts_as_indexed :fields => [:venue_name, :cost, :description, :link]
 
   validates :venue_name, :presence => true, :uniqueness => true
   
   has_friendly_id :date_and_name, :use_slug => true
+  
+  def title
+    "#{starts_at.strftime '%b %d'} #{venue_name}"
+  end
   
   private
   
