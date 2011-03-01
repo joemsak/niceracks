27e09c39ec5a2cd9d2154b17bde644eac32d0a44
Joe Sak <joe@joesak.com>
2011-02-28 20:57:20 -0600

    Ladies are saved with friendly IDs

--- vendor/engines/ladies/app/models/lady.rb
+++ vendor/engines/ladies/app/models/lady.rb
@@ -4,5 +4,7 @@ class Lady < ActiveRecord::Base
 
   validates :name, :presence => true, :uniqueness => true
   
+  has_friendly_id :name, :use_slug => true, :approximate_ascii => true, :strip_non_ascii => true
+  
   belongs_to :photo, :class_name => 'Image'
 end
