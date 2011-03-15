bcbe1226a86f5670866b6b0b39fecfdf343fc2d1
Joe Sak <joe@joesak.com>
2011-03-14 21:19:17 -0500

    try a title?

--- vendor/engines/photos/app/models/photo.rb
+++ vendor/engines/photos/app/models/photo.rb
@@ -5,4 +5,9 @@ class Photo < ActiveRecord::Base
   acts_as_indexed :fields => [:caption]
   
   belongs_to :picture, :class_name => 'Image'
+  
+  def title
+    caption || picture.file_name
+  end
+  
 end
