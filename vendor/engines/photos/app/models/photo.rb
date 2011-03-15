4547cca6f0e2a981161ab5771c14b63afe2e3684
Joe Sak <joe@joesak.com>
2011-03-14 21:18:43 -0500

    whups, supposed to make caption not required

--- vendor/engines/photos/app/models/photo.rb
+++ vendor/engines/photos/app/models/photo.rb
@@ -3,8 +3,6 @@ class Photo < ActiveRecord::Base
   acts_as_taggable
   
   acts_as_indexed :fields => [:caption]
-
-  validates :caption, :presence => true, :uniqueness => true
   
   belongs_to :picture, :class_name => 'Image'
 end
